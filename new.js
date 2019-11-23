2e1d6baa10f00ed2498be121d925ac85d5b777e3
Vipul <vipulpathak113@gmail.com>
2019-11-24 03:25:09 +0530

    Added bootstrap

--- new.js
+++ new.js
@@ -1,42 +1,39 @@
 function deleteAllHistory() {
-  var items = localStorage.getItem("item_to_delete");
-  console.log(items);
-
-  var newest= localStorage.getItem("time_period")|| "last_hour"
-  console.log(newest);
-  switch (newest)  
-  { 
-     case "last_hour": 
-     sinceTime= (new Date()).getTime() - 3600000 
-         break; 
-     case "last_24hour": 
-     sinceTime= (new Date()).getTime() - 86400000
-         break; 
-     case "last_7days": 
-     sinceTime= (new Date()).getTime() - 604800000 
-         break; 
-     case "last_4weeks": 
-     sinceTime= (new Date()).getTime() - 2419200000  
-         break;
-     case "all_time": 
-     sinceTime= "" 
-         break;           
-     default: 
-     sinceTime= (new Date()).getTime() - 3600000 
+  var items = localStorage.getItem("item_to_delete") || '{"cache": true}';
+  var newest = localStorage.getItem("time_period") || "last_hour";
+  switch (newest) {
+    case "last_hour":
+      sinceTime = new Date().getTime() - 3600000;
+      break;
+    case "last_24hour":
+      sinceTime = new Date().getTime() - 86400000;
+      break;
+    case "last_7days":
+      sinceTime = new Date().getTime() - 604800000;
+      break;
+    case "last_4weeks":
+      sinceTime = new Date().getTime() - 2419200000;
+      break;
+    case "all_time":
+      sinceTime = "";
+      break;
+    default:
+      sinceTime = new Date().getTime() - 3600000;
   }
 
   chrome.browsingData.remove(
-    { since:sinceTime,
+    {
+      since: sinceTime,
       originTypes: {
         protectedWeb: true,
-        unprotectedWeb: true, 
+        unprotectedWeb: true,
         extension: true
       }
     },
     JSON.parse(items),
 
     function() {
-      console.log("Data is Deleted...");
+      window.close();
     }
   );
 }
--- options.js
+++ options.js
@@ -1,66 +1,58 @@
-window.addEventListener("load",function(){
-var itemstoget= JSON.parse(localStorage.getItem("item_to_delete")) || {cache: true}
-console.log(itemstoget)
+window.addEventListener("load", function() {
+  var itemstoget = JSON.parse(localStorage.getItem("item_to_delete")) || {
+    cache: true
+  };
 
-var timePeriod=localStorage.getItem("time_period");
+  var timePeriod = localStorage.getItem("time_period") || "last_hour";
 
-document.querySelector('option[value=" + timePeriod +"]').selected = true
+  document.querySelector(`option[value="${timePeriod}"]`).selected = true;
 
-var keys = Object.keys(itemstoget);
-console.log(keys)
+  var keys = Object.keys(itemstoget);
 
-var inputElements = document.getElementsByTagName("input");
+  var inputElements = document.getElementsByTagName("input");
 
-if(keys){
-for(var i=0;i<keys.length; i++){
-for (var j = 0; inputElements[j]; j++) {
-  if (inputElements[j].id===keys[i]) {
-    inputElements[j].checked=true
+  if (keys) {
+    for (var i = 0; i < keys.length; i++) {
+      for (var j = 0; inputElements[j]; j++) {
+        if (inputElements[j].id === keys[i]) {
+          inputElements[j].checked = true;
+        }
+      }
+    }
   }
-}
-}
-
-}
-
-})
+});
 
 var checkBoxes = document.getElementById("checkboxes");
-checkBoxes.addEventListener("click", function (e) {
-console.log(e.target.id)
+checkBoxes.addEventListener("click", function(e) {
   var checkedValue = null;
-  var checkitem = JSON.parse(localStorage.getItem("item_to_delete"))|| {cache: true};
+  var checkitem = JSON.parse(localStorage.getItem("item_to_delete")) || {
+    cache: true
+  };
 
-  var abc= Object.entries(checkitem)
-  console.log(abc)
+  var newItem = Object.entries(checkitem);
   var newobject = {};
 
-    if (e.target.checked) {
-      checkedValue = e.target.id;
-      abc.push([checkedValue, e.target.checked]);
-      console.log(abc)
-      newobject = Object.fromEntries(abc);
-  localStorage.setItem("item_to_delete", JSON.stringify(newobject));
+  if (e.target.checked) {
+    checkedValue = e.target.id;
+    newItem.push([checkedValue, e.target.checked]);
+    newobject = Object.fromEntries(newItem);
+    localStorage.setItem("item_to_delete", JSON.stringify(newobject));
+  } else {
+    for (var i = 0; i < newItem.length; i++) {
+      if (
+        newItem[i][0] == [e.target.id, true][0] &&
+        newItem[i][1] == [e.target.id, true][1]
+      ) {
+        newItem.splice(i, 1);
+        newobject = Object.fromEntries(newItem);
+        localStorage.setItem("item_to_delete", JSON.stringify(newobject));
+      }
     }
-    else{
-      console.log(abc)
-      console.log([e.target.id, true])
-
-      for ( var i = 0; i < abc.length; i++ ) {
-        if ( abc[i][0] == [e.target.id, true][0] && abc[i][1] == [e.target.id, true][1] ) {
-            console.log(i);
-            abc.splice(i,1)
-            console.log(abc)
-            newobject = Object.fromEntries(abc);
-  localStorage.setItem("item_to_delete", JSON.stringify(newobject));
-        }
-    }
-    }
-
-
+  }
 });
 
 const activities = document.getElementById("time_period");
 
 activities.addEventListener("change", e => {
-  localStorage.setItem("time_period",e.target.value)
+  localStorage.setItem("time_period", e.target.value);
 });
